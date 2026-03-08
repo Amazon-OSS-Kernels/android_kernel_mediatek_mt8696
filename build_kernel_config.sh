dacafbe7c05c352f95481b9ad0b8747e13f5dd30
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 14:33:47 -0500

    Base release import: FireTVStick_4K_Max_2nd_Gen-8.1.0.1-20231208

---
+++ build_kernel_config.sh
@@ -0,0 +1,25 @@
+################################################################################
+#
+#  build_kernel_config.sh
+#
+#  Copyright (c) 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+################################################################################
+
+KERNEL_SUBPATH="kernel/mediatek/mt8696/5.10"
+DEFCONFIG_NAME="karat_defconfig"
+TARGET_ARCH="arm64"
+TOOLCHAIN_REPO="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
+TOOLCHAIN_BRANCH="llvm-r383902b"
+TOOLCHAIN_NAME="aarch64-linux-android-4.9"
+TOOLCHAIN_PREFIX="aarch64-linux-android-"
+PARALLEL_EXECUTION="-j24"
+
+# Expected image files are seperated with ":"
+KERNEL_IMAGES="arch/arm64/boot/Image:arch/arm64/boot/Image.gz:arch/arm64/boot/Image.gz-dtb"
+
+################################################################################
+# NOTE: You must fill in the following with the path to a copy of Clang compiler
+# Recommended r416183b.
+################################################################################
+CLANG_COMPILER_PATH=""
